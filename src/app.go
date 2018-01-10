2eae07542805dad237df9d2d90342a5d5ca59c91
Billy <billy.braga@gmail.com>
2018-01-09 22:28:07 -0500

    service

--- src/app.go
+++ src/app.go
@@ -14,12 +14,7 @@ type Item struct {
 	Description string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	t, e := template.ParseFiles("views/home.html")
-	if e != nil {
-		w.Write([]byte(e.Error()))
-		return
-	}
+func handler(w http.ResponseWriter, r *http.Request, t *template.Template) {
 	if t.Tree == nil || t.Tree.Root == nil {
 		w.Write([]byte("Template null"))
 		return
@@ -42,15 +37,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		json.Unmarshal(file, &data[i])
 	}
 
-	e = t.Execute(w, data)
+	e := t.Execute(w, data)
 	if e != nil {
-		w.Write([]byte(e.Error()))
+		panic(e)
 		return
 	}
 }
 
 func main() {
 	//http.Handle("/", gziphandler.GzipHandler(http.HandlerFunc(handler)))
-	http.HandleFunc("/", handler)
+
+	t, e := template.ParseFiles("views/home.html")
+	if e != nil {
+		panic(e)
+		return
+	}
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		handler(w, r, t)
+	})
 	http.ListenAndServe(":8081", nil)
 }

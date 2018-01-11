df1a2eee1072e1ced52741cf733dbe9528d8cc2f
Billy <billy.braga@gmail.com>
2018-01-11 00:27:51 -0500

    perf

--- src/app.go
+++ src/app.go
@@ -5,30 +5,50 @@ import (
 	//"io/ioutil"
 	"fmt"
 	//"github.com/pkg/profile"
-	"io"
-	"os"
+	//"io"
+	//"os"
+	"io/ioutil"
 )
 
-func handler(ctx *fasthttp.RequestCtx) {
+type FileManager struct {
+	files [3][]byte
+}
+
+func NewFileManager() *FileManager {
+	f := new(FileManager)
+
+	for i := 0; i < 3; i++ {
+		content, err := ioutil.ReadFile(fmt.Sprintf("./data/list%d.json", (i%3)+1))
+		if err != nil {
+			panic(err)
+			return f
+		}
+		f.files[i] = content
+	}
+
+	return f
+}
+
+type Handler struct {
+	fileManager *FileManager
+}
+
+func NewHandler() *Handler {
+	h := new(Handler)
+	h.fileManager = NewFileManager()
+	return h
+}
+
+func (h *Handler) handle(ctx *fasthttp.RequestCtx) {
 	defer ctx.SetConnectionClose()
 
 	for i := 0; i < 100; i++ {
-		fileReader, err := os.Open(fmt.Sprintf("./data/list%d.json", (i % 3) + 1))
-		if err != nil {
-			panic(err)
-			return
-		}
-		defer fileReader.Close()
-
-		_, err = io.Copy(ctx.Response.BodyWriter(), fileReader)
-		if err != nil {
-			panic(err)
-			return
-		}
+		ctx.Response.AppendBody(h.fileManager.files[i % 3])
 	}
 }
 
 func main() {
 	//defer profile.Start(profile.CPUProfile).Stop()
-	fasthttp.ListenAndServe(":8081", handler)
+	handler := NewHandler()
+	fasthttp.ListenAndServe(":8081", handler.handle)
 }
